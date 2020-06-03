c2ab316ce5758f7f96976be01f4de5eef5c05cfa
cesar <dev@cesar.sh>
2020-06-03 18:50:14 +0100

    make extension compatible for Chrome
    Required changing namespace "browser" to "chrome". Now works in Firefox
    and Chrome.

--- src/main.js
+++ src/main.js
@@ -1,36 +1,36 @@
 const shortUrlElement = document.getElementById('shortUrl')
-// const baseDomain = 'https://linkea.me'
-const baseDomain = 'http://localhost:3000'
+const baseDomain = 'https://linkea.me'
 
-function shortenUrl(tabs) {
-  const url = tabs[0].url // safe to assume only one result
+const shortenUrl = (tabs) => {
+  const url = tabs[0].url
   const apiUrl = baseDomain + '/api?u=' + encodeURIComponent(url)
 
-  browser.storage.local.get()
-    .then((res) => {
-      if (!res[url]) {
-        fetch(apiUrl, {
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'X-Csrf-Token': 'token', // TODO add UUID
-          },
+  chrome.storage.local.get(res => {
+    if (!res[url]) {
+      fetch(apiUrl, {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'X-Csrf-Token': 'token', // TODO add UUID
+        },
+      })
+        .then(response => response.json())
+        .then(data => {
+          if (data.data) {
+            const obj = {}
+            obj[url] = data.data
+            chrome.storage.local.set(obj)
+            shortUrlElement.value = data.data
+          } else if (data.error) {
+            shortUrlElement.value = data.error
+          } else {
+            shortUrlElement.value = 'hubo un error :('
+          }
         })
-          .then(response => response.json())
-          .then(data => {
-            if (data.data) {
-              const obj = {}
-              obj[url] = data.data
-              browser.storage.local.set(obj)
-              shortUrlElement.value = data.data
-            } else {
-              shortUrlElement.value = 'hubo un error :('
-            }
-          })
-      } else {
-        shortUrlElement.value = res[url]
-      }
-    })
+    } else {
+      shortUrlElement.value = res[url]
+    }
+  })
 }
 
-browser.tabs.query({ currentWindow: true, active: true }).then(shortenUrl)
+chrome.tabs.query({ active: true, currentWindow: true }, shortenUrl)
