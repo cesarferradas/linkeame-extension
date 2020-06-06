5f2503479d73a9e18b7f99871d5d307a77596e2a
cesar <dev@cesar.sh>
2020-06-06 21:25:56 +0100

    add copy to clipboard

--- src/main.js
+++ src/main.js
@@ -1,36 +1,55 @@
-const shortUrlElement = document.getElementById('shortUrl')
+let state = {}
+
 const baseDomain = 'https://linkea.me'
 
-const shortenUrl = (tabs) => {
+const shortUrl = document.getElementById('url')
+const copyCheckbox = document.getElementById('copy')
+
+const copy = () => {
+  if (state.autoCopy) {
+    const copyText = shortUrl.innerHTML
+    navigator.clipboard.writeText(copyText)
+  }
+}
+
+const shorten = (tabs) => {
   const url = tabs[0].url
   const apiUrl = baseDomain + '/api?u=' + encodeURIComponent(url)
 
-  chrome.storage.local.get(res => {
-    if (!res[url]) {
-      fetch(apiUrl, {
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-          'X-Csrf-Token': 'token', // TODO add UUID
-        },
+  if (!state[url]) {
+    fetch(apiUrl, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'X-Csrf-Token': 'token',
+      },
+    })
+      .then((res) => res.json())
+      .then((payload) => {
+        if (payload.data) {
+          const obj = {}
+          obj[url] = payload.data
+          chrome.storage.local.set(obj)
+          shortUrl.innerHTML = payload.data
+          copy()
+        } else {
+          shortUrl.innerHTML = payload.error || 'Error'
+        }
       })
-        .then(response => response.json())
-        .then(data => {
-          if (data.data) {
-            const obj = {}
-            obj[url] = data.data
-            chrome.storage.local.set(obj)
-            shortUrlElement.value = data.data
-          } else if (data.error) {
-            shortUrlElement.value = data.error
-          } else {
-            shortUrlElement.value = 'hubo un error :('
-          }
-        })
-    } else {
-      shortUrlElement.value = res[url]
-    }
-  })
+  } else {
+    shortUrl.innerHTML = state[url]
+    copy()
+  }
 }
 
-chrome.tabs.query({ active: true, currentWindow: true }, shortenUrl)
+copyCheckbox.addEventListener('change', () => {
+  state.autoCopy = copyCheckbox.checked
+  chrome.storage.local.set({ autoCopy: state.autoCopy })
+  copy()
+})
+
+chrome.storage.local.get((store) => {
+  state = store
+  copyCheckbox.checked = state.autoCopy
+  chrome.tabs.query({ active: true, currentWindow: true }, shorten)
+})
